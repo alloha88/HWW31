744eb45b19147f222db16247bc3a1329eaa51880
Timurgess <timurgess@yandex.ru>
2022-07-11 13:08:46 +0300

    Задание_1

---
+++ HWW22/Задание_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+double powerNumber(int x, int y)
+{
+    double power = Math.Pow(x, y);
+    return power;
+}
+
+Console.WriteLine("Введите число A и число B");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число A возведенное в степень B равняется" +powerNumber(numberA, numberB));
+
