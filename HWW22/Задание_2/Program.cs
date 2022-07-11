811068002c6b2bec017f25dd7d254d28345298d2
Timurgess <timurgess@yandex.ru>
2022-07-11 13:09:01 +0300

    Задание_2

---
+++ HWW22/Задание_2/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int sumNumbers (int x)
+{
+    int sum = 0;
+    while(x > 0)
+    {
+        int y = x%10;
+        x = x/10;
+        sum = sum + y;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма цифр в введенном числе равняется" + sumNumbers(number));
