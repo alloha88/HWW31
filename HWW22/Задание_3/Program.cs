64528d4e028a2e875d71abde89fa29ec036b14e5
Timurgess <timurgess@yandex.ru>
2022-07-11 13:09:29 +0300

    Задание_3

---
+++ HWW22/Задание_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу которая задаёт массив из 8 элементов и выводит их на экран.
+
+void fillArray (int[] arr)
+{
+    int length = arr.Length;
+    for (int index = 0; index < length; index++)
+        {
+            arr[index] = new Random().Next(1,100);
+        }
+}
+
+int[] array = new int[8];
+
+fillArray(array);
+
+Console.Write("[ ");
+Console.Write(string.Join(" | ", array));
+Console.WriteLine(" ]");
